46fb183a6c8690ae6388b4b002d51216b1ab4484
YuNing Shen <yuning.shen@moying.ai>
2022-10-25 20:16:47 +0800

    feat: new tun configure

--- pkg/utils/tun.go
+++ pkg/utils/tun.go
@@ -3,6 +3,7 @@
 package utils
 
 import (
+	"github.com/sirupsen/logrus"
 	"golang.org/x/sys/unix"
 	"os"
 	"sync"
@@ -20,6 +21,8 @@ type TUNConfigure struct {
 	errors chan error
 	events chan int
 
+	interfaceName string
+
 	// the device was passed IFF_NO_PI
 	nopi        bool
 	netlinkSock int
@@ -33,4 +36,20 @@ type TUNConfigure struct {
 	closeOnce sync.Once
 	nameOnce  sync.Once
 	nameError error
+}
+
+func NewTUNConfigure(tunName string) *TUNConfigure {
+	tfp, err := os.OpenFile(tunDevicePoint, unix.O_RDWR|unix.O_CLOEXEC, 0)
+	if err != nil {
+		logrus.Fatalf("interface %s create channel error", tunName)
+	}
+
+	return &TUNConfigure{
+		tunFp:         tfp,
+		interfaceName: tunName,
+	}
+}
+
+func (tun *TUNConfigure) File() *os.File {
+	return tun.tunFp
 }
